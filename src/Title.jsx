fad44c844d3fae825faacf016ea517a4f5536295
Dushyant-2004 <dushyantvasisht@gmail.com>
2025-02-10 20:22:29 +0530

    first commit

---
+++ src/Title.jsx
@@ -0,0 +1,13 @@
+function Title() {
+    
+    let name="Dushyant";
+    return (
+    <div> 
+       <p>2*2= {2*2}</p>
+       <h1>I am the title !</h1>
+       <p>Hi, {name.toUpperCase()}</p>
+        </div>
+        );
+  }
+
+  export default Title;
