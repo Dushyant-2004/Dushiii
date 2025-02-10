12bfaf81e9f1c3de1bcd2a66955369ed226f38e9
Dushyant-2004 <dushyantvasisht@gmail.com>
2025-02-10 21:06:05 +0530

    add a h3 tag

--- src/Title.jsx
+++ src/Title.jsx
@@ -6,8 +6,11 @@ function Title() {
        <p>2*2= {2*2}</p>
        <h1>I am the title !</h1>
        <p>Hi, {name.toUpperCase()}</p>
+       <h3>my self dushiii</h3>
         </div>
         );
   }
+
+
 
   export default Title;
